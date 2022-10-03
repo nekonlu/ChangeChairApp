51cfb11568ca1429c0b6f5bad4426afa34637e2f
nekonlu <appyoohoy@gmail.com>
2022-10-03 19:13:52 +0900

    initial commit

---
+++ ChangeChair/ChangeChairApp.swift
@@ -0,0 +1,18 @@
+//
+//  ChangeChairApp.swift
+//  ChangeChair
+//
+//  Created by nekonlu on 2022/10/03.
+//  
+//
+
+import SwiftUI
+
+@main
+struct ChangeChairApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
